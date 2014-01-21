723571c1d888485a3c554db6310af28a5f768f7a
kenjis <kenji.uui@gmail.com>
2014-01-21 09:10:05 +0900

    Fix array() to []

--- config/routes.phalcon.php
+++ config/routes.phalcon.php
@@ -9,20 +9,20 @@
 $router = new \Phalcon\Mvc\Router();
 $router->add(
     '/:controller/:action/:params',
-    array(
+    [
         'controller' => 1,
         'action'     => 2,
         'params'     => 3,
-    )
+    ]
 );
 
 // top page
 $router->add(
     '/',
-    array(
+    [
         'controller' => 'Hello',
         'action'     => 'index',
-    )
+    ]
 );
 
 return $router;
