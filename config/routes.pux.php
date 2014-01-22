7263be34987374ddb594cadf502487f25bcb2c5b
kenjis <kenji.uui@gmail.com>
2014-01-22 16:26:02 +0900

    Fix comment

--- config/routes.pux.php
+++ config/routes.pux.php
@@ -3,10 +3,10 @@
  * Routing for Pux
  * 
  * config/routes.pux.mux.php is a compiled file of this file.
- * If you want to use mux.php for speed, you must set ORE_USE_MUX environment variable.
- * See public/.htaccess.
- * And you must update config/routes.pux.mux.php manually, if you change this file.
- * You can do it with update-mux.sh.
+ * If you want to use routes.pux.mux.php for speed, you must set ORE_USE_MUX 
+ * environment variable. See public/.htaccess.
+ * And you must update config/routes.pux.mux.php manually, if you change this 
+ * file. You can do it with update-mux.sh.
  * 
  * Documentaion: https://github.com/c9s/Pux
  */
